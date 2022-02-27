3e82d92f51c0d89008791d54a8d6a737db39444b
Ozgur TEZEL <ozgurtezel89@gmail.com>
2022-02-27 22:37:27 +0000

    Add Getting random user from random User API with fetch method

--- RANDOM-USER-GEN/app.js
+++ RANDOM-USER-GEN/app.js
@@ -16,6 +16,17 @@ const app = Vue.createApp({
             this.email = 'tillytezel89@gmail.com';
             this.gender = 'female';
             this.picture = 'https://randomuser.me/api/portraits/lego/9.jpg'
+        },
+        async getRandomUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json();
+            const result = results[0];
+
+            this.firstname = result.name.first;
+            this.lastname = result.name.last;
+            this.email = result.email;
+            this.gender = result.gender;
+            this.picture = result.picture.large
         }
     }
 })
